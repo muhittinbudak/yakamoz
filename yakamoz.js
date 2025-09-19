c9ff880b12456c37cdebdac4f41129474330b75f
muhittin budak <mistermuhittin@gmail.com>
2025-09-19 14:31:24 +0300

    Create yakamoz.js
    js

---
+++ yakamoz.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+
+
+
+
+$("#dugme").click(function (e) { 
+
+alert("Merhaba Üzümcü IĞdır.");
+    //$("#sonuc").text("eik");
+    //h3 id="tarih"
+    //  e.preventDefault();
+});
+
+/*    
+$("button").click(function(){ 
+
+$.post("demo_test_post.asp",{ name: "Donald Duck", city: "Duckburg" }, function(data,status){ alert("Data: " + data + "\nStatus: " + status); }); 
+
+});
+$("#sonuc").text("metin");
+$http({ method:"GET",url:"./test.php" }).then(function (response){ $scope.Results =response.data.deste; },function(response){ $scope.statuscode = response.status; });
+  
+*/
+
+
+
+}); // end jquery
