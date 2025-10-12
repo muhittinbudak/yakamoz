2927daeae54f0640751e327141741d1319a8f9e0
muhittin budak <mistermuhittin@gmail.com>
2025-10-12 23:31:11 +0300

    Create girilenoyakhaber.js

---
+++ girilenoyakhaber.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){  
+// BENİM GİRDİĞİM OYAK OBP YAZILARI  
+  
+      function haberleriYukle() {
+
+        $.ajax({
+          url: "https://muhittinbudak.github.io/oyakbilgi/yeni.json", // PHP API URL'n
+          method: "GET",
+          dataType: "json",
+          success: function(data) {
+            let html = '<ul class="media-list main-list" style="border-top: 2px solid red;padding:0" >';
+            if (data.length > 0) {
+
+              data.forEach(function(haber, index) { 
+
+
+            html += "<li class='media'><a class='pull-left' href='" + haber.obp_weburl +"'><div class='image-container'><img class='media-object img-responsive img-rounded' src='"+ haber.obp_resim_url +"'></div></a><div class='media-body'><div class='media-heading strait-16 listitem'>" + haber.obp_title + "</div><div class='listitem by-author strait-13'>" + haber.obp_title + "</div><div class='btn-toolbar' style='margin-top:10px;'><a href='"+ haber.obp_weburl + "' class='btn btn-lg btn-primary habertext strait'>Habere Git <span class='glyphicon glyphicon-chevron-right'></span></a></div></li>";         
+              });
+
+            } else {
+              html += "<li>Henüz haber yok.</li>";
+            }
+            html += "</ul>";
+            $("#haberler").html(html);
+          },
+          error: function() {
+            $("#haberler").html("<p>Could not get data.</p>");
+          }
+        });    
+      } // haberleriYukle() bitiş     
+
+ haberleriYukle();  
+}); 
